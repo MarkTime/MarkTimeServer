dd07e5b797ad58ea84b2f0eff19d7dde04c09f44
John Board <johnrobboard@gmail.com>
2016-01-29 15:21:13 +1000

    Minimum Viable Product for SQLite

--- api/uploadNew.php
+++ api/uploadNew.php
@@ -2,6 +2,13 @@
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], "/home/marktime/MarkTimeServer/MarkTime.db")) {
         file_put_contents("status", "uploaded\n");
         echo "success";
+
+        $command = escapeshellcmd("python ../main.py");
+        $output = shell_exec($command);
+        echo $output;
+
+        file_put_contents("log", $output);
+
     } else{
         echo "fail";
     }
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python
 import sqlite3
 import os.path
+import sys
+import logging
+import traceback
 
 class Application:
     def __init__(self):
@@ -8,8 +10,16 @@ class Application:
         """
         If master db doesn't exist, make the upload the master, and move on.
         """
+        self.dir = "/home/marktime/MarkTimeServer/"
+        os.chdir(self.dir)
+
+        self.updateStatus("processing")
+
         if not os.path.isfile("MarkTimeMaster.db"):
             os.rename("MarkTime.db", "MarkTimeMaster.db")
+            f1 = open("api/status", "w")
+            f1.write("finished\n");
+            f1.close()
             return
         """
         Else...
@@ -18,19 +28,41 @@ class Application:
         self.conn = sqlite3.connect("MarkTimeMaster.db")
         self.c = self.conn.cursor()
 
-        self.copyTable("attendance", "attendance1")
-        self.execSQL("INSERT INTO attendance1 (boyID, attendance) VALUES (3, 2)")
-        self.mergeTables("attendance", "attendance1")
-        print("Merged stuff.")
-        return
-
         self.execSQL("ATTACH DATABASE 'MarkTime.db' as upload;")
         self.execSQL("BEGIN;")
-        self.execSQL("INSERT INTO boys (boyName, boySquad) SELECT boyName, boySquad FROM upload.boys;")
+        for table in self.getTables():
+
+            cols = self.getColumns(table)
+            if len(cols)>1:
+                k = cols[1]
+                self.mergeTables(table, "upload."+table, key=k)
+
+        deletes = []
+        for row in self.c.execute("SELECT * FROM changelog"):
+            print(row)
+            if row[3] == "DEL":
+                    deletes.append("DELETE FROM '"+row[1]+"' WHERE _id="+row[2])
+        for sql in deletes:
+            self.c.execute(sql)
+            logger.info(sql)
+            print(sql)
+        self.c.execute("DELETE FROM changelog")
+
+
         self.execSQL("END;")
         self.execSQL("DETACH upload;" )
-
+        self.conn.commit()
         self.conn.close()
+
+        os.remove("MarkTime.db")
+        self.updateStatus("finished")
+
+
+
+    def updateStatus(self, status):
+        f1 = open("api/status", "w")
+        f1.write(status+"\n")
+        f1.close()
 
     """
     Copies unique entries in tableB into tableA
@@ -47,10 +79,17 @@ class Application:
         print(key)
         sql = "INSERT INTO "+tableA+" ("+",".join(cols)+") SELECT "+",".join(cols)+" FROM "+tableB+" WHERE "+key+" NOT IN (SELECT "+key+" FROM "+tableA+")"
         self.execSQL(sql)
+        self.conn.commit()
 
-        #self.c.execute("INSERT INTO "+tableA+" ("+(len(cols)*"?,")[:-1]+") "+
-        #                "SELECT "+(len(cols)*"?,")[:-1]+" FROM "+tableB+" WHERE "+key+
-        #                " NOT IN (SELECT "+key+" FROM "+tableA+");", (cols*2))
+    def mergeTables(self, tableA, tableB, key="", keys=[]):
+        print("Merging tables.")
+        cols = self.getColumns(tableA)
+        del cols[0]
+        if key == "":
+            key = cols[0]
+
+        sql = "INSERT INTO "+tableA+" ("+",".join(cols)+") SELECT "+",".join(cols)+" FROM "+tableB+" WHERE "+key+" NOT IN (SELECT "+key+" FROM "+tableA+")"
+        self.execSQL(sql)
         self.conn.commit()
 
     def copyTable(self, tableName, newTableName):
@@ -86,5 +125,17 @@ class Application:
     def setFlag(self, flag):
         pass
 
+def my_handler(exctype, value, tb):
+    logger.exception("""
+    Type: {}
+    Value: {}
+    Traceback: {}
+    """.format(exctype, value, traceback.extract_tb(tb)))
+    self.updateStatus("error")
+
 if __name__ == "__main__":
+    logging.basicConfig(filename='python.log',level=logging.DEBUG)
+    logger = logging.getLogger('mylogger')
+    sys.excepthook = my_handler
+
     app = Application()
